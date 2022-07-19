b7c1e232a60aa466c9c08490d3aaba7df66dbd36
Pedro-Augusto-Barbosa-Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2022-07-18 21:45:53 -0300

    test url shared

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,15 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content='Konan Amigo Secreto' />
+        <meta property='og:description' content={`Venha brincar com seus amigos no velho amigo secreto!`} />
+        <meta property='og:image' content="" />
+        <meta property='og:image:width' content='' />
+        <meta property='og:image:height' content="" />
+        <meta property='og:locale' content="pt_BR" />
+        <meta property='og:url' content={`https://Konan.vercel.app/room/register-me`} />
+        <meta property='og:site_name' content='Konan' />
         <title>Amigos secreto de konoha</title>
       </Head>
       <AuthProvider>
