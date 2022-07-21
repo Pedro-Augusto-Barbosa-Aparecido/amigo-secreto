63b20e2939785e07e94964b8a012776ed4f1c8a3
Pedro-Augusto-Barbosa-Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2022-07-20 21:06:01 -0300

    change bg color and font

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -7,13 +7,13 @@ type SpinnerProps = {
 
 export default function Spinner (props: SpinnerProps) {
     return (
-        <div className="absolute text-center flex flex-col items-center justify-center top-0 left-0 h-full w-full">
+        <div className="absolute bg-gray-back-100 bg-opacity-80 text-center flex flex-col items-center justify-center top-0 left-0 h-full w-full">
             <TailSpin 
                 color="#F00"
                 width={100}
                 height={100}
             /> 
-            <span className="text-sm text-gray-700 mt-8">{ props.messageSpan + "..."}</span>
+            <span className="text-xl font-semibold text-black mt-8">{ props.messageSpan + "..."}</span>
         </div>
     );
 
