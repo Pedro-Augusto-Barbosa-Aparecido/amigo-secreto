b187348daba96295777bc47ded34df4f33e72d4d
Pedro-Augusto-Barbosa-Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2022-07-27 19:16:54 -0300

    make 500 error page

---
+++ src/pages/500.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { IoMdArrowRoundForward } from "react-icons/io";
+
+export default function NotFoundPage () {
+    return (
+        <main className="w-full h-with-nav">
+            <div className="w-full h-full">
+                <Image 
+                    src={"/imgs/500-image-opt-one.jpg"}
+                    layout="fill"
+                    objectFit="fill"
+                    priority
+                />
+                <div className="absolute text-white font-istok-web p-16 w-full flex flex-col h-with-nav">
+                    <div className="text-white text-6xl mb-16">
+                        <span>Código </span>
+                        <span className="text-dark-orange-600 font-bold">500!</span>
+                    </div>
+                    <p className="text-3xl w-5/12">
+                        Desculpe por esse incômodo. Aconteceu algo com o nosso servidor ou você foi desconectado, verifique sua rede e tente mais tarde.
+                    </p>
+                    <div className="flex hover:cursor-pointer items-center mt-24 py-4 pl-8 hover:underline bg-gray-200 bg-opacity-10 font-normal  hover:bg-opacity-80 active:cursor-default hover:bg-gray-back-400 rounded text-md w-2/12">
+                        <Link href="/">
+                            <a>
+                                Voltar para página inicial.
+                            </a>
+                        </Link>
+                        <IoMdArrowRoundForward 
+                            color="white"
+                            className="ml-8 text-2xl"
+                        />
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+}
